ddd84d9041a4e3abc3a6b1dd8d638ada8bbafd27
VictorTao1998 <jit124@ucsd.edu>
2022-01-28 22:59:50 -0800

    first

--- run_test.sh
+++ run_test.sh
@@ -2,7 +2,7 @@
 export PYTHONWARNINGS="ignore"
 
 python -m torch.distributed.launch /jianyu-fast-vol/reprojection_finetune/test_psmnet_reprojection.py \
---config-file '/jianyu-fast-vol/reprojection_finetune/configs/remote_test_on_real.yaml' \
+--config-file '/jianyu-fast-vol/reprojection_finetune/configs/remote_test.yaml' \
 --model '/jianyu-fast-vol/eval/reprojection_finetune/train_reprojection_finetune/models/model_0.pth' \
 --output '/jianyu-fast-vol/eval/reprojection_finetune/test_reprojection_finetune/' \
 --onreal \
