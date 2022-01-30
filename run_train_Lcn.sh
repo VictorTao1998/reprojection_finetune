6d8787049c3ecab7891a31958278436b1cfac4da
VictorTao1998 <jit124@ucsd.edu>
2022-01-30 01:43:15 -0800

    lcn

--- run_train_Lcn.sh
+++ run_train_Lcn.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export PYTHONWARNINGS="ignore"
 
-python -m torch.distributed.launch /jianyu-fast-vol/reprojection_finetune/train_psmnet_temporal_ir_reproj_lcn.py \
+python -m torch.distributed.launch /jianyu-fast-vol/reprojection_finetune/train_psmnet_temporal_ir_reproj_Gwc_lcn.py \
 --config-file '/jianyu-fast-vol/reprojection_finetune/configs/remote_train_primitive_randscenes.yaml' \
 --logdir '/jianyu-fast-vol/eval/reprojection_finetune/train_reprojection_Lcn/' \
 --gaussian-blur \
