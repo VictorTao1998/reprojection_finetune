80eda974ef628bf7df18f31b557b454b77f7063d
VictorTao1998 <jit124@ucsd.edu>
2022-01-30 22:33:23 -0800

    lcn

--- run_train_Gwc.sh
+++ run_train_Gwc.sh
@@ -3,6 +3,6 @@ export PYTHONWARNINGS="ignore"
 
 python -m torch.distributed.launch /jianyu-fast-vol/reprojection_finetune/train_psmnet_temporal_ir_reproj_Gwc.py \
 --config-file '/jianyu-fast-vol/reprojection_finetune/configs/remote_train_primitive_randscenes.yaml' \
---logdir '/jianyu-fast-vol/eval/reprojection_finetune/train_reprojection_Gwc_2/' \
+--logdir '/jianyu-fast-vol/eval/reprojection_gwc/train_reprojection_gwc/' \
 --gaussian-blur \
 --color-jitter
