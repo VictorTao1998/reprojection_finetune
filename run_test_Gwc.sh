e94cd27c5f42aaaac8907af142fa3c66d9db9215
VictorTao1998 <jit124@ucsd.edu>
2022-01-30 22:03:08 -0800

    lcn

--- run_test_Gwc.sh
+++ run_test_Gwc.sh
@@ -3,7 +3,7 @@ export PYTHONWARNINGS="ignore"
 
 python -m torch.distributed.launch /jianyu-fast-vol/reprojection_finetune/test_psmnet_reprojection_Gwc.py \
 --config-file '/jianyu-fast-vol/reprojection_finetune/configs/remote_test.yaml' \
---model '/jianyu-fast-vol/eval/reprojection_finetune/train_reprojection_Lcn/models/model_18000.pth' \
---output '/jianyu-fast-vol/eval/reprojection_finetune/test_reprojection_Gwc_lcn/' \
+--model '/jianyu-fast-vol/eval/reprojection_finetune/train_reprojection_Gwc_sim/models/model_40000.pth' \
+--output '/jianyu-fast-vol/eval/reprojection_finetune/test_reprojection_Gwc_sim/' \
 --onreal \
 --exclude-bg 
