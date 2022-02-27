5f92d101b320d748eb1237ded5abc92c5e88d679
VictorTao1998 <jit124@ucsd.edu>
2022-02-26 18:08:35 -0800

    test

--- run_test.sh
+++ run_test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export PYTHONWARNINGS="ignore"
 
-python -m torch.distributed.launch /jianyu-fast-vol/ActiveZero/test_psmnet_sim.py \
+python /jianyu-fast-vol/ActiveZero/test_psmnet_sim.py \
 --config-file '/jianyu-fast-vol/ActiveZero/configs/remote_test.yaml' \
 --model '/jianyu-fast-vol/eval/sim_cv_bce_2/train_sim_cv_bce/models/model_100000.pth' \
 --output '/jianyu-fast-vol/eval/sim_cv_bce_2/test_sim_cv_bce' \
