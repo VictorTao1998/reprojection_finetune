f987a269cd4d2ff33effdad2eba16fdec6754453
VictorTao1998 <jit124@ucsd.edu>
2022-02-26 19:03:32 -0800

    test

--- run_test.sh
+++ run_test.sh
@@ -3,7 +3,7 @@ export PYTHONWARNINGS="ignore"
 
 python /jianyu-fast-vol/ActiveZero/test_psmnet_sim.py \
 --config-file '/jianyu-fast-vol/ActiveZero/configs/remote_test.yaml' \
---model '/jianyu-fast-vol/eval/sim_cv_bce_2/train_sim_cv_bce_weight/models/model_100000.pth' \
---output '/jianyu-fast-vol/eval/sim_cv_bce_2/test_sim_cv_bce_weight' \
+--model '/jianyu-fast-vol/eval/sim_cv_adv_2/train_sim_adv/models/model_100000.pth' \
+--output '/jianyu-fast-vol/eval/sim_cv_adv_2/test_sim_adv' \
 --exclude-bg \
 --exclude-zeros 
