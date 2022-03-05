fc15854b2045db9ea28cdf6b50caf1177f9e8a25
VictorTao1998 <jit124@ucsd.edu>
2022-03-05 04:31:56 -0800

    deep

--- run_train_sim.sh
+++ run_train_sim.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 export PYTHONWARNINGS="ignore"
 
-python -m torch.distributed.launch /jianyu-fast-vol/ActiveZero/train_psmnet_sim_cv_adversarial.py \
+python -m torch.distributed.launch /jianyu-fast-vol/ActiveZero/train_psmnet_sim_cv_bce.py \
 --config-file '/jianyu-fast-vol/ActiveZero/configs/remote_train_primitive_randscenes.yaml' \
---logdir '/jianyu-fast-vol/eval/loss_function/sim_adv' \
+--logdir '/jianyu-fast-vol/eval/loss_function/sim_bce' \
 --gaussian-blur \
 --color-jitter
