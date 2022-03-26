db4d8eede58be5d394efb97ce242472aa1b90806
VictorTao1998 <jit124@ucsd.edu>
2022-03-25 20:27:07 -0700

    depth

--- run_train_sim.sh
+++ run_train_sim.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 export PYTHONWARNINGS="ignore"
 
-python -m torch.distributed.launch /jianyu-fast-vol/ActiveZero/train_psmnet_sim.py \
+python -m torch.distributed.launch --nproc_per_node=2 /jianyu-fast-vol/ActiveZero/train_psmnet_sim.py \
 --config-file '/jianyu-fast-vol/ActiveZero/configs/remote_train_primitive_randscenes.yaml' \
 --logdir '/jianyu-fast-vol/eval/psm_depth/vanilla' \
 --gaussian-blur \
 --color-jitter \
---usedepth 
+--usedepth \
+--summary-freq 100
