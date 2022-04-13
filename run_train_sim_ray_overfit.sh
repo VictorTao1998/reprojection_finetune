8489365a46fb81d85994e5c9de27ecfebb504919
VictorTao1998 <jit124@ucsd.edu>
2022-04-12 23:09:18 -0700

    test

--- run_train_sim_ray_overfit.sh
+++ run_train_sim_ray_overfit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export PYTHONWARNINGS="ignore"
 
-python -m torch.distributed.launch /code/ActiveZero/train_psmnet_sim_ray_overfit.py \
+python -m torch.distributed.launch /jianyu-fast-vol/ActiveZero/train_psmnet_sim_ray_overfit.py \
 --config-file '/jianyu-fast-vol/ActiveZero/configs/remote_train_primitive_randscenes.yaml' \
 --logdir '/jianyu-fast-vol/eval/psm_ray/' \
 --gaussian-blur \
